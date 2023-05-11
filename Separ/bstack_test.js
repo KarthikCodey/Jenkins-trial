647b6669a307803f6592d2deb5ba37efb193a279
KarthikCodey <129168907+KarthikCodey@users.noreply.github.com>
2023-05-11 18:35:03 +0530

    Update bstack_test.js

--- Separ/bstack_test.js
+++ Separ/bstack_test.js
@@ -27,6 +27,9 @@ describe('BrowserStack Login Scenarios', function() {
     await submitButton.click();
 
     await driver.sleep(3000);
+    const errorMessageText =  await driver.findElement(By.css('div[class="error-msg show"] span[aria-live="polite"]')).getText();
+    //const errorMessageText =  await driver.findElement(By.css('#user_email_login + .error-msg')).getText();
+    assert.equal(errorMessageText, "Invalid Email"); 
 
     await username.clear();
     await password.clear();
@@ -43,7 +46,8 @@ describe('BrowserStack Login Scenarios', function() {
     await submitButton.click();
 
     await driver.sleep(3000);
-
+    const errorMessageText1 =  await driver.findElement(By.css('#user_password + .error-msg')).getText();
+    assert.equal(errorMessageText1, "Invalid password");
     await username.clear();
     await password.clear();
   });
