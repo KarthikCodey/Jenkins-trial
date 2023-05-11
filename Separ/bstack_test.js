87c79b5477141f3fb38ae1b766c6f0851b902e32
KarthikCodey <129168907+KarthikCodey@users.noreply.github.com>
2023-05-12 00:47:51 +0530

    Update bstack_test.js

--- Separ/bstack_test.js
+++ Separ/bstack_test.js
@@ -43,7 +43,8 @@ describe('BrowserStack Login Scenarios', function() {
     let password = await driver.findElement(By.css('#user_password'));
     await password.sendKeys('Shane$800');
 
-    let submitButton = await driver.wait(until.elementLocated(By.xpath('/html//input[@id='user_submit']'),10000)).click();
+    //let submitButton = await driver.wait(until.elementLocated(By.xpath('/html//input[@id='user_submit']'),10000)).click();
+    const submitButton = await driver.wait(until.elementLocated(By.xpath('/html//input[@id="user_submit"]')), 10000).click();
     //await submitButton.click();
 
     await driver.sleep(10000);
