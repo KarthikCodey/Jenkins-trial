5536c87d5c7a3c162453ac0c2358067a20086ffd
KarthikCodey <129168907+KarthikCodey@users.noreply.github.com>
2023-05-17 17:21:20 +0530

    Update sampletest.conf.js

--- android/conf/sampletest.conf.js
+++ android/conf/sampletest.conf.js
@@ -14,7 +14,7 @@ const parallelConfig = {
         testObservability: true,
         browserstackLocal: false,
         opts: { forcelocal: false, localIdentifier: "webdriverio-appium-app-browserstack-repo" },
-        app: process.env.BROWSERSTACK_APP_PATH || 'BStackSampleApp.ipa',
+        app: process.env.BROWSERSTACK_APP_PATH || 'wikipedia.zip',
         testObservabilityOptions: {
             'projectName': 'BrowserStack Samples',
             'buildName': 'Sample App',
